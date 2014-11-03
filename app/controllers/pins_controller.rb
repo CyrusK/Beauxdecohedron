3d0e33addd3eee94ec45286fd6e4d44fccd3c59b
Cyrus Khajvandi <cskhajvandi@gmail.com>
2014-11-03 00:38:33 -0800

    Updated authorization so users can't delete another user's listings, made changes to pins controller, pins view in index and show and used devise user authentication

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,38 +1,42 @@
 class PinsController < ApplicationController
-  respond_to :html, :xml, :json
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @pins = Pin.all
-    respond_with(@pins)
   end
 
   def show
-    respond_with(@pin)
   end
 
   def new
-    @pin = Pin.new
-    respond_with(@pin)
+    @pin = current_user.pins.build
   end
 
   def edit
   end
 
   def create
-    @pin = Pin.new(pin_params)
-    @pin.save
-    respond_with(@pin)
+    @pin = current_user.pins.build(pin_params)
+    if @pin.save
+      redirect_to @pin, notice: 'Listing was successfully created.'
+    else
+      render action: 'new'
+    end
   end
 
   def update
-    @pin.update(pin_params)
-    respond_with(@pin)
+    if @pin.update(pin_params)
+      redirect_to @pin, notice: 'Pin was successfully updated.'
+    else
+      render action: 'edit'
+    end
   end
 
   def destroy
     @pin.destroy
-    respond_with(@pin)
+    redirect_to pins_url, notice: 'Pin was successfully destroyed.'
   end
 
   private
@@ -40,7 +44,12 @@ class PinsController < ApplicationController
       @pin = Pin.find(params[:id])
     end
 
+ def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "Not authorized to edit this listing." if @pin.nil?
+    end
+
     def pin_params
       params.require(:pin).permit(:description)
     end
-end
+  end
