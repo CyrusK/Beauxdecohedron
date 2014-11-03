4e2d7cb83f5298673bca0c9919665af6006c0d05
Cyrus Khajvandi <cskhajvandi@gmail.com>
2014-11-03 03:38:30 -0800

    Added validation type to iamge file type under Pins model

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,5 +2,6 @@ class Pin < ActiveRecord::Base
     belongs_to :user
 
     has_attached_file :image, :styles => { :medium => "400x400>", :thumb => "200x200>" }
+    validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
 end
