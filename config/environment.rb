4f5a03326b443467a1ecde3616eae5b1d6b7c032
Cyrus Khajvandi <cskhajvandi@gmail.com>
2014-11-07 03:26:01 -0800

    made changes to environment

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require File.expand_path('../application', __FILE__)
 ActionMailer::Base.smtp_settings = {
   :user_name => 'info@monisr.com',
   :password => '123321',
-  :domain => 'www.incentru.com',
+  :domain => '122.10.90.37',
   :address => 'smtp.122.10.90.37',
   :port => 2525,
   :authentication => :plain,
