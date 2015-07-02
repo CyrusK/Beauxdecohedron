3b2fc946861599e86db92695b555fee79573fcf5
J. R. Pickhardt <pickhardt@gmail.com>
2015-07-02 13:51:34 -0700

    Add pin_id to the contact email

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -6,8 +6,9 @@ class ContactController < ApplicationController
     last = params[:last]
     phone = params[:phone]
     country = params[:country]
+    pin_id = params[:pin_id]
     
-    ContactMailer.contact_email(name, email, body, last, phone, country).deliver
+    ContactMailer.contact_email(name, email, body, last, phone, country, pin_id).deliver
     redirect_to contact_path, notice: 'Message sent'
   end
 end
--- app/models/contact_mailer.rb
+++ app/models/contact_mailer.rb
@@ -1,14 +1,15 @@
 class ContactMailer < ActionMailer::Base
   default to: # my email address
   
-  def contact_email(name, email, body, last, phone, country)
+  def contact_email(name, email, body, last, phone, country, pin_id)
     @name = name
     @email = email
     @body = body
     @last = last
     @phone = phone
     @country = country
-    
+    @pin_id = pin_id
+
     mail(from: email, subject: 'Contact Request', to: 'info@incentru.com')
 
   end
